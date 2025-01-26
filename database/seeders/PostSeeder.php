c9ae9e6eb897a514d6888b34f72ab69260c81213
takara-umehara <tkr5.nn3@icloud.com>
2025-01-26 06:22:15 +0000

    Pagination have been completed

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -37,5 +37,89 @@ class PostSeeder extends Seeder
             'importance' => '5',
             'user_id' => '2',
         ]);
+
+        DB::table('posts')->insert([
+            'title' => 'その3',
+            'body' => '三つ目',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+            'deadline_dateTime' => '20251231235959',
+            'completed_dateTime' => null,
+            'category_id' => '1',
+            'importance' => '5',
+            'user_id' => '1',
+        ]);
+
+        DB::table('posts')->insert([
+            'title' => 'その4',
+            'body' => '四つ目',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+            'deadline_dateTime' => '20251231235959',
+            'completed_dateTime' => null,
+            'category_id' => '2',
+            'importance' => '5',
+            'user_id' => '2',
+        ]);
+
+        DB::table('posts')->insert([
+            'title' => 'その5',
+            'body' => '五つ目',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+            'deadline_dateTime' => '20251231235959',
+            'completed_dateTime' => null,
+            'category_id' => '2',
+            'importance' => '5',
+            'user_id' => '2',
+        ]);
+
+        DB::table('posts')->insert([
+            'title' => 'その6',
+            'body' => '六つ目',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+            'deadline_dateTime' => '20251231235959',
+            'completed_dateTime' => null,
+            'category_id' => '2',
+            'importance' => '5',
+            'user_id' => '2',
+        ]);
+
+        DB::table('posts')->insert([
+            'title' => 'その7',
+            'body' => '七つ目',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+            'deadline_dateTime' => '20251231235959',
+            'completed_dateTime' => null,
+            'category_id' => '2',
+            'importance' => '5',
+            'user_id' => '2',
+        ]);
+
+        DB::table('posts')->insert([
+            'title' => 'その8',
+            'body' => '八つ目',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+            'deadline_dateTime' => '20251231235959',
+            'completed_dateTime' => null,
+            'category_id' => '2',
+            'importance' => '5',
+            'user_id' => '2',
+        ]);
+
+        DB::table('posts')->insert([
+            'title' => 'その9',
+            'body' => '九つ目',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+            'deadline_dateTime' => '20251231235959',
+            'completed_dateTime' => null,
+            'category_id' => '2',
+            'importance' => '5',
+            'user_id' => '2',
+        ]);
     }
 }
